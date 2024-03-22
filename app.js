0d38739739dbc38fcd14cef2fdb65c2434c6bb9e
Rashid Ahmad <rashid.ahmad@live.com>
2024-03-22 17:34:00 +0500

    1. Implemented response handling using a callback func

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const url = require('url');
 const http = require('http');
-const templateHelper = require('./helpers/template')
+const templateHelper = require('./helpers/template');
 
 //The middleware
 app.use(function (req, res, next) {
@@ -10,35 +10,36 @@ app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     next();
 });
-app.use(express.json());
 //End of middleware
 
 const PORT = process.env.PORT || 3000;
 
 http.createServer((req, res) => {
-    
-    const queryData = url.parse(req.url, true).query;
-    let formattedDomainsList = '';
-    if (queryData && queryData['address']) {
-        //console.log(queryData['address']);
-        formattedDomainsList = templateHelper.formatDomainList(queryData['address']);
-    }
+    try {
+        const queryData = url.parse(req.url, true).query;
+        let domains = '';
+        if (queryData && queryData['address']) {
+            domains = queryData['address'];
+        }
 
-    if (req.url.includes('/I/want/title')) {
-        res.writeHead(200, { 'Content-Type': 'text/html' });
-        let template = templateHelper.getPageTemplate(formattedDomainsList);
-        res.end(template);
-    } else {
-        res.writeHead(404, `Not Found`);
+        if (req.url.includes('/I/want/title')) {
+            res.writeHead(200, { 'Content-Type': 'text/html' });
+            templateHelper.getPageTemplate(domains, (template, err) => {
+                if (err === null) {
+                    res.end(template);
+                } else {
+                    res.writeHead(500, `Internal Server Error ${err && err.message || null}`);
+                    res.end();
+                }
+            });
+        } else {
+            res.writeHead(404, `Not Found`);
+            res.end();
+        }
+    } catch (e) {
+        res.writeHead(500, `Internal Server Error ${e && e.message || null}`);
         res.end();
     }
-    //app.use('/', require('./controllers/titlecontroller'));
 }).listen(PORT, () => {
     console.log(`Server started using the port ${PORT}`);
 });
-
-
-
-/*app.listen(PORT, () => {
-    console.log(`Server started using the port ${PORT}`);
-});*/
--- helpers/template.js
+++ helpers/template.js
@@ -1,34 +1,37 @@
 let validations = require('./validators');
 
-function getPageTemplate(domainsStr) {
-    return `
-    <html>
-    <head></head>
-    <body>
-     <h1> Following are the titles of given websites: </h1>
-     <ul>
-        ${domainsStr}
-     </ul>
-    </body>
-    </html>
-    `;
-}
-
-function formatDomainList(domains) {
-    let domainStr = '';
-    if (typeof(domains) === 'string') {
-        if (!validations.validateDomainName(domains)) {
-            domainStr = `<li>${domains} - NO RESPONSE</li>`;
-        } else {
-            domainStr = `<li>${domains}</li>`;
+function getPageTemplate(domains, callback) {
+    try {
+        let domainStr = '';
+        if (typeof(domains) === 'string') {
+            if (!validations.validateDomainName(domains)) {
+                domainStr = `<li>${domains} - NO RESPONSE</li>`;
+            } else {
+                domainStr = `<li>${domains}</li>`;
+            }
+        } else if (Array.isArray(domains)) {
+            for(let i = 0; i < domains.length; i++) {
+                domainStr += !validations.validateDomainName(domains[i]) ? `<li>${domains[i]} - NO RESPONSE</li>` : `<li>${domains[i]}</li>`;
+            }
         }
-    } else if (Array.isArray(domains)) {
-        for(let i = 0; i < domains.length; i++) {
-            domainStr += !validations.validateDomainName(domains[i]) ? `<li>${domains[i]} - NO RESPONSE</li>` : `<li>${domains[i]}</li>`;
-            console.log(domainStr);
-        }
-    }
-    return domainStr;
+        //Using timeout only to simulate the db call or a file read
+        setTimeout(() => {
+            let template = `
+                    <html>
+                    <head></head>
+                    <body>
+                    <h1> Following are the titles of given websites: </h1>
+                    <ul> 
+                        ${domainStr}
+                    </ul>
+                    </body>
+                    </html>
+                    `;
+            callback(template, null);
+        }, 1000);
+    } catch(e) {
+        callback('', e);
+    } 
 }
 
 module.exports = {getPageTemplate, formatDomainList};
